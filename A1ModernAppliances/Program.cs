ac039e55a7c4d6e9ef260486159852ea04ddd5ef
Ashleen Sidhu <100872112+AshleenSidhu@users.noreply.github.com>
2024-06-05 22:35:13 -0600

    Changes to MyModernAppliances
    Changes to  Checkout, Find, DisplayRefriderators, and DisplayVacuum

---
+++ A1ModernAppliances/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace A1ModernAppliances
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
